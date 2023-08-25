d5ce1b023803cdf20fb1e758b3f2537d1761b130
Kenny Burgher <kenny.burgher@gmail.com>
2023-08-25 11:18:46 -0700

    Time: 66 ms (85.83%) | Memory: 41.9 MB (97.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -1,29 +1,40 @@
+// Solution 2 with whiteboard notes & pseudocode
+
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
+  all letters in s can be rearranged to get t and vice versa
+  do spaces count? 
+  empty string? 
+  max length? 
+  case sensitive? 
+
+  create an array of length 26 and fill with zeros
+    as s,t grow this array uses constant memory!
+    both s,t are built out of a common base, namely the alphabet
+
+  iterate over s
+    use charcodes and shift them to 0-25
+    array at charcode at index of s minus charcode of 'a' gets incremented
+    array at charcode at index of t minus charcode of 'a' gets decremented
+  
+  if array has any non-zero value return false
+  else true
  */
-var isAnagram = function (s, t) {
-    if (s.length != t.length) return false;
+const isAnagram = function(s, t) {
+    
+    if (s.length !== t.length) return false; 
 
-    sCharCount = makeFreqCounter(s);
-    tCharCount = makeFreqCounter(t);
+    const frequency = new Array(26).fill(0)
 
-    for (let key in sCharCount) {
-        if (sCharCount[key] != tCharCount[key]) {
-            return false;
-        }
+    for (let i = 0; i < s.length; i++) {
+        frequency[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        frequency[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
     }
 
+    for (const count of frequency) {
+        if (count !== 0) return false;
+    }
     return true;
 };
-
-function makeFreqCounter(iter) {
-    let freqCounter = {}
-
-    for (let elem of iter) {
-        freqCounter[elem] = (freqCounter[elem] || 0) + 1;
-    }
-
-    return freqCounter;
-}
