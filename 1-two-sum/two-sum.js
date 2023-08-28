dff7ab95dc601bb55ed1f4521562328f93256867
Kenny Burgher <kenny.burgher@gmail.com>
2023-08-28 12:25:42 -0700

    Time: 50 ms (95.72%) | Memory: 43.1 MB (31.61%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,11 +5,27 @@
  */
 const twoSum = function (nums, target) {
 
-    const unique = new Set(nums);
+    // time complexity optimized solution
+        // checks for solutions along the way of building the hash
+    let map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-        if (unique.has(target - nums[i]) && i !== nums.indexOf(target - nums[i])) {
-            return [i, nums.indexOf(target - nums[i])];
+        let complement = target - nums[i];
+        
+        if (map.has(complement)) {
+            return [i, map.get(complement)];
         }
+        
+        map.set(nums[i], i);
     }
+
+    // space complexity optimized solution
+        // nested loop uses no memory other than the list of nums
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = i + 1; j < nums.length; j++ ) {
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j];
+    //         }
+    //     }
+    // }
 };
