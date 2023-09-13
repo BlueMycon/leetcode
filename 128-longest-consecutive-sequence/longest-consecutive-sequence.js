540f7f37bbc9b6442cb9d49463e521b6d107219c
Kenny Burgher <kenny.burgher@gmail.com>
2023-09-12 18:45:54 -0700

    Time: 247 ms (35.29%) | Memory: 62.6 MB (58.49%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestConsecutive = function (nums) {
+    let uniqueNums = new Set(nums);
+
+    let maxCount = 0;
+    for (let num of nums) {
+        if (uniqueNums.has(num - 1)) continue;
+        
+        let count = 1;
+        let elem = num;
+        while (uniqueNums.has(elem + 1)) {
+            count++;
+            elem++
+        }
+
+        if (count > maxCount) maxCount = count;
+    }
+
+    return maxCount;
+};
+
